1629310d7ac62b3327d780e281dee3735fa01a22
Ben Ashford <benashford@users.noreply.github.com>
2017-06-04 14:15:31 +0100

    Use Stream and Sink only for the first iteration

--- src/client.rs
+++ src/client.rs
@@ -13,53 +13,39 @@ use super::resp;
 /// TODO: comeback and optimise this number
 const DEFAULT_BUFFER_SIZE:usize = 100;
 
-
 fn connect(addr: &SocketAddr, core: &Core) -> Box<Future<Item=ClientConnection, Error=io::Error>> {
     let handle = core.handle();
     let client_con = TcpStream::connect(addr, &handle).map(move |socket| {
         let framed = socket.framed(resp::RespCodec);
         let (write_f, read_f) = framed.split();
         let write_b = write_f.buffer(DEFAULT_BUFFER_SIZE);
-        let (write_sync_tx, write_sync_rx) = mpsc::unbounded();
-        let sending = write_sync_rx.fold(write_b, |write_b, resp_val| {
-            write_b.send(resp_val).map_err(|_| ())
-        });
-        handle.spawn(sending.map(|_| ()));
         ClientConnection {
-            sender: ClientSend(write_sync_tx),
-            receiver: ClientRecv(Box::new(read_f))
+            sender: ClientSink(Box::new(write_b)),
+            receiver: ClientStream(Box::new(read_f))
         }
     });
     Box::new(client_con)
 }
 
-struct ClientSend(mpsc::UnboundedSender<resp::RespValue>);
-
-impl ClientSend {
-    /// Sends a Redis command to Redis
-    ///
-    /// TODO - check return values to contain leaky abstractions
-    fn send(&self, cmd: resp::RespValue) -> Result<(), mpsc::SendError<resp::RespValue>> {
-        mpsc::UnboundedSender::send(&self.0, cmd)
-    }
-}
 
 /// TODO - is the boxing necessary?  It makes the type signature much simpler
-struct ClientRecv(Box<Stream<Item=resp::RespValue, Error=io::Error>>);
+struct ClientSink(Box<Sink<SinkItem=resp::RespValue, SinkError=io::Error>>);
+struct ClientStream(Box<Stream<Item=resp::RespValue, Error=io::Error>>);
 
 /// A low-level client connection representing a sender and a receiver.
 ///
 /// The two halves operate independently from one another
-///
-/// TODO: whether the receiver should be a straight Stream or not...
 struct ClientConnection {
-    sender: ClientSend,
-    receiver: ClientRecv
+    sender: ClientSink,
+    receiver: ClientStream
 }
 
 #[cfg(test)]
 mod test {
-    use futures::{Future, Stream};
+    use std::io;
+
+    use futures::{Future, Sink, Stream};
+    use futures::stream;
 
     use tokio_core::reactor::Core;
 
@@ -71,10 +57,11 @@ mod test {
         let addr = "127.0.0.1:6379".parse().unwrap();
 
         let connection = super::connect(&addr, &core).and_then(|connection| {
-            connection.sender.send(["PING", "TEST"].as_ref().into()).unwrap();
-            connection.receiver.0.take(1).collect()
+            let a = connection.sender.0.send(["PING", "TEST"].as_ref().into());
+            let b = connection.receiver.0.take(1).collect();
+            a.join(b)
         });
-        let values = core.run(connection).unwrap();
+        let (_, values) = core.run(connection).unwrap();
         assert_eq!(values.len(), 1);
         assert_eq!(values[0], "TEST".into());
     }
@@ -84,14 +71,16 @@ mod test {
         let mut core = Core::new().unwrap();
         let addr = "127.0.0.1:6379".parse().unwrap();
         let connection = super::connect(&addr, &core).and_then(|connection| {
-            connection.sender.send(["FLUSH"].as_ref().into()).unwrap();
-            (0..1000).map(|i| {
-                connection.sender.send(["SADD", "test_set", &format!("VALUE: {}", i)].as_ref().into())
-            }).fold((), |_, r| r.unwrap());
-            connection.sender.send(["SMEMBERS", "test_set"].as_ref().into()).unwrap();
-            connection.receiver.0.skip(1001).take(1).collect()
+            let mut ops = Vec::<resp::RespValue>::new();
+            ops.push(["FLUSH"].as_ref().into());
+            ops.extend((0..1000).map(|i| ["SADD", "test_set", &format!("VALUE: {}", i)].as_ref().into()));
+            ops.push(["SMEMBERS", "test_set"].as_ref().into());
+            let ops_r:Vec<Result<resp::RespValue, io::Error>> = ops.into_iter().map(Result::Ok).collect();
+            let send = connection.sender.0.send_all(stream::iter(ops_r));
+            let receive = connection.receiver.0.skip(1001).take(1).collect();
+            send.join(receive)
         });
-        let values = core.run(connection).unwrap();
+        let (_, values) = core.run(connection).unwrap();
         assert_eq!(values.len(), 1);
         let values = match &values[0] {
             &resp::RespValue::Array(ref values) => values.clone(),
