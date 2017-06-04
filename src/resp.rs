20457a56226b8c8017fca1255f0aaf9b6eba71ec
Ben Ashford <benashford@users.noreply.github.com>
2017-06-04 06:57:47 +0100

    Refactoring

--- src/resp.rs
+++ src/resp.rs
@@ -1,10 +1,10 @@
 use std::io;
 
-use bytes::BytesMut;
+use bytes::{BufMut, BytesMut};
 
 use tokio_io::codec::{Decoder, Encoder};
 
-// TODO - this stuff - all options
+// TODO - this stuff - all Resp types
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum RespValue {
     Array(Vec<RespValue>),
@@ -36,12 +36,52 @@ impl<T: ToResp> From<T> for RespValue {
 /// Codec to read frames
 pub struct RespCodec;
 
+#[inline]
+fn write_rn(buf: &mut BytesMut) {
+    buf.put_u8(b'\r');
+    buf.put_u8(b'\n');
+}
+
+#[inline]
+fn check_and_reserve(buf: &mut BytesMut, amt: usize) {
+    let remaining_bytes = buf.remaining_mut();
+    if remaining_bytes < amt {
+        buf.reserve(amt);
+    }
+}
+
+#[inline]
+fn write_header(symb: u8, len: usize, buf: &mut BytesMut) {
+    let len_as_string = len.to_string();
+    let len_as_bytes = len_as_string.as_bytes();
+    let header_bytes = 1 + len_as_bytes.len() + 2;
+    check_and_reserve(buf, header_bytes);
+    buf.put_u8(symb);
+    buf.extend(len_as_bytes);
+    write_rn(buf);
+}
+
 impl Encoder for RespCodec {
     type Item = RespValue;
     type Error = io::Error;
 
-    fn encode(&mut self, msg: RespValue, buf: &mut BytesMut) -> io::Result<()> {
-        unimplemented!()
+    fn encode(&mut self, msg: RespValue, buf: &mut BytesMut) -> Result<(), Self::Error> {
+        match msg {
+            RespValue::Array(ary) => {
+                write_header(b'*', ary.len(), buf);
+                for v in ary {
+                    self.encode(v, buf)?;
+                }
+            }
+            RespValue::BulkString(bstr) => {
+                let len = bstr.len();
+                write_header(b'$', len, buf);
+                check_and_reserve(buf, len + 2);
+                buf.extend(bstr);
+                write_rn(buf);
+            }
+        }
+        Ok(())
     }
 }
 
@@ -49,7 +89,7 @@ impl Decoder for RespCodec {
     type Item = RespValue;
     type Error = io::Error;
 
-    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<RespValue>, io::Error> {
+    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         unimplemented!()
     }
 }
